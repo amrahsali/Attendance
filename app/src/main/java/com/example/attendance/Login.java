6ef16b508e6f036e68381888987029bbca7558b3
amrah <Saliamrah300@gmail.com>
2023-03-29 16:27:16 +0100

    login successfully working

--- app/src/main/java/com/example/attendance/Login.java
+++ app/src/main/java/com/example/attendance/Login.java
@@ -1,14 +1,99 @@
 package com.example.attendance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    Button button;
+    private EditText userNameEdt, passwordEdt;
+    private FirebaseAuth mAuth;
+    private ProgressBar loadingPB;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        // initializing all our variables.
+        userNameEdt = findViewById(R.id.username);
+        passwordEdt = findViewById(R.id.password);
+        // newUserTV = findViewById(R.id.idTVNewUser);
+        mAuth = FirebaseAuth.getInstance();
+        loadingPB = findViewById(R.id.idPBLoading);
+        button = findViewById(R.id.loginbtn);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // hiding our progress bar.
+                loadingPB.setVisibility(View.VISIBLE);
+                // getting data from our edit text on below line.
+                String email = userNameEdt.getText().toString();
+                String password = passwordEdt.getText().toString();
+                // on below line validating the text input.
+                if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                    Toast.makeText(Login.this, "Please enter your credentials..", Toast.LENGTH_SHORT);
+                    return;
+                }
+                // on below line we are calling a sign in method and passing email and password to it.
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // on below line we are checking if the task is success or not.
+                        if (task.isSuccessful()) {
+                            // on below line we are hiding our progress bar.
+                            loadingPB.setVisibility(View.GONE);
+                            Toast.makeText(Login.this, "Login Successful..", Toast.LENGTH_SHORT).show();
+                            // on below line we are opening our mainactivity.
+                            Intent i = new Intent(Login.this, MainActivity.class);
+                            startActivity(i);
+                          finish();
+                        } else {
+                            // hiding our progress bar and displaying a toast message.
+                            loadingPB.setVisibility(View.GONE);
+                            Toast.makeText(Login.this, "Please enter valid user credentials..", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // in on start method checking if
+        // the user is already sign in.
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            // if the user is not null then we are
+            // opening a main activity on below line.
+            Intent i = new Intent(Login.this, MainActivity.class);
+            finish();
+        }
+    }
+
+//amrah@gmail.com password amrahsali12
 }
