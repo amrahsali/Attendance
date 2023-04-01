2081854469df216720335324954af9470e921c81
amrah <Saliamrah300@gmail.com>
2023-04-02 00:18:17 +0100

    could not work on the nav

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -26,35 +26,6 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawer = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                 switch (menuItem.getItemId()){
-
-                     case R.id.add_staff:
-                          Intent intent = new Intent(MainActivity.this, First_screen.class);
-                     startActivity(intent);
-
-                     case R.id.add_student:
-                         Intent intent2 = new Intent(MainActivity.this, Add_studentActivity.class);
-                         startActivity(intent2);
-                     case R.id.navigation_record:
-                         Intent intent3 = new Intent(MainActivity.this, View_recordActivity.class);
-                         startActivity(intent3);
-                     case R.id.navigation_exams:
-                         Intent intent4 = new Intent(MainActivity.this, ExaminationActivity.class);
-                         startActivity(intent4);
-                     /*case R.id.navigation_courses:
-                         Intent intent = new Intent(MainActivity.this, First_screen.class);
-                         startActivity(intent);*/
-                 }
-
-                return true;
-            }
-        });
-
 
 
 
