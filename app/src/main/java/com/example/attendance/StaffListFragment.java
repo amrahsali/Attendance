c81a781de7b68d969375ad321a5127478e3556b1
GEORGE <georgequin19@gmail.com>
2023-05-07 01:02:05 +0100

    updated staff list

--- app/src/main/java/com/example/attendance/StaffListFragment.java
+++ app/src/main/java/com/example/attendance/StaffListFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,10 +21,16 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 
 public class StaffListFragment extends Fragment {
@@ -41,6 +49,10 @@ public class StaffListFragment extends Fragment {
     TextView name,email;
     private static final int ADD_COURSE_REQUEST = 1;
     private RecyclerView coursesRV;
+    private StaffRVModal staffRVModal;
+    private StaffAdapter staffAdapter;
+    private ArrayList<StaffRVModal> staffRVModalArrayList;
+
 
 
 
@@ -70,10 +82,15 @@ public class StaffListFragment extends Fragment {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Staff");
         FloatingActionButton fab = view.findViewById(R.id.idFABAdd);
+        staffRVModalArrayList = new ArrayList<>();
         mStorageref = FirebaseStorage.getInstance().getReference("Upload Photos");
+        staffAdapter = new StaffAdapter(staffRVModalArrayList, getContext());
         coursesRV = view.findViewById(R.id.idRVCourses);
         coursesRV.setLayoutManager(new LinearLayoutManager(getContext()));
         coursesRV.setHasFixedSize(true);
+        coursesRV.setAdapter(staffAdapter);
+        getStaff();
+
 
         // adding on click listener for floating action button.
         fab.setOnClickListener((View v) -> {
@@ -86,6 +103,51 @@ public class StaffListFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void getStaff() {
+        //on below line clearing our list.
+        staffRVModalArrayList.clear();
+        Query query = databaseReference.orderByChild("userID").equalTo(FirebaseAuth.getInstance().getUid());
+        //on below line we are calling add child event listener method to read the data.
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //on below line we are hiding our progress bar.
+                //loadingPB.setVisibility(View.GONE);
+                //adding snapshot to our array list on below line.
+                staffRVModalArrayList.add(snapshot.getValue(StaffRVModal.class));
+                //notifying our adapter that data has changed.
+                staffAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //this method is called when new child is added we are notifying our adapter and making progress bar visibility as gone.
+                //loadingPB.setVisibility(View.GONE);
+                staffAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                //notifying our adapter when child is removed.
+                staffAdapter.notifyDataSetChanged();
+                //loadingPB.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //notifying our adapter when child is moved.
+                staffAdapter.notifyDataSetChanged();
+                //loadingPB.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
