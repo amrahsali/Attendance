e362868237b852a6a8e1503001323825e6af965a
amrah <Saliamrah300@gmail.com>
2023-05-11 23:33:10 +0100

    Exams Rv

---
+++ app/src/main/java/com/example/attendance/ExamsRecord.java
@@ -0,0 +1,36 @@
+package com.example.attendance;
+
+public class ExamsRecord {
+    public  String nameExams;
+    public  String titleExams;
+    public  String dateExams;
+
+
+    public String getNameExams() {
+        return nameExams;
+    }
+
+    public void setNameExams(String nameExams) {
+        this.nameExams = nameExams;
+    }
+
+    public String getTitleExams() {
+        return titleExams;
+    }
+
+    public void setTitleExams(String titleExams) {
+        this.titleExams = titleExams;
+    }
+
+    public String getDateExams() {
+        return dateExams;
+    }
+
+    public void setDateExams(String dateExams) {
+        this.dateExams = dateExams;
+    }
+
+    public ExamsRecord(){
+
+    }
+}
--- app/src/main/java/com/example/attendance/Exams_record_Activity.java
+++ app/src/main/java/com/example/attendance/Exams_record_Activity.java
@@ -1,14 +1,49 @@
 package com.example.attendance;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Exams_record_Activity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ExamsRecordAdapter adapter; // Create an object of the Adapter class
+    private DatabaseReference mbase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams_record);
+
+        mbase = FirebaseDatabase.getInstance().getReference();
+        recyclerView = findViewById(R.id.rvExamsRecord);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<ExamsRecord> options =
+                new FirebaseRecyclerOptions.Builder<ExamsRecord>()
+                        .setQuery(mbase, ExamsRecord.class)
+                        .build();
+
+
+        adapter = new ExamsRecordAdapter(options);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
