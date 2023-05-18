68157ea3e8dddce808083a72d7472f88bf9a1e23
amrah <Saliamrah300@gmail.com>
2023-05-18 10:58:55 +0100

    Grid view

--- app/src/main/java/com/example/attendance/CoursesFragment.java
+++ app/src/main/java/com/example/attendance/CoursesFragment.java
@@ -1,5 +1,7 @@
 package com.example.attendance;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -16,6 +20,7 @@ import java.util.List;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -28,12 +33,43 @@ public class CoursesFragment extends Fragment {
     GridView courseGV;
     ArrayList<Coursemodal> coursemodalArrayList;
     FirebaseFirestore db;
+    CourseAdapter adapter; // Declare adapter as an instance variable
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private void collectDataAndDisplay(String courseName, String courseCode) {
+        // Create a new Coursemodal object and set the course name and course code
+        Coursemodal course = new Coursemodal();
+        course.setCourseName(courseName);
+        course.setCodeName(courseCode);
+
+        coursemodalArrayList.add(course);
+
+        // Notify the adapter that the data has changed
+        adapter.notifyDataSetChanged();
+
+        // Save the course details to Firebase Firestore
+        saveCourseToFirestore(courseName, courseCode);
 
     }
+    private void saveCourseToFirestore(String courseName, String courseCode) {
+        // Create a new document in the "Data" collection with the course details
+        db.collection("Data")
+                .add(new Coursemodal(courseName, courseCode))
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Course saved successfully
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to save course
+                    }
+                });
+    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,6 +87,17 @@ public class CoursesFragment extends Fragment {
         // here we are calling a method
         // to load data in our list view.
         loadDatainGridView();
+
+        FloatingActionButton addButton = view.findViewById(R.id.courseFABtn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogBox();
+            }
+        });
+        adapter = new CourseAdapter(getActivity(), coursemodalArrayList);
+        courseGV.setAdapter(adapter);
+
 
         return view;
 
@@ -98,6 +145,24 @@ public class CoursesFragment extends Fragment {
                         Toast.makeText(getActivity(),  "Fail to load data..", Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    private void showDialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Course");
+        View dialogView = LayoutInflater.from(getActivity()).inflate(R.layout.activity_dialog_box_course2, null);
+        final EditText courseNameEditText = dialogView.findViewById(R.id.edt_name);
+        final EditText courseCodeEditText = dialogView.findViewById(R.id.edt_faculty);
+        builder.setView(dialogView);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String courseName = courseNameEditText.getText().toString().trim();
+                String courseCode = courseCodeEditText.getText().toString().trim();
+                collectDataAndDisplay(courseName, courseCode);
+            }
+        });
+        builder.show();
     }
 }
 
---
+++ app/src/main/java/com/example/attendance/DialogBox_courseActivity.java
@@ -0,0 +1,27 @@
+package com.example.attendance;
+
+import static android.app.PendingIntent.getActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class DialogBox_courseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_box_course2);
+    }
+
+    public void showDialogBox(View view) {
+
+
+}
+}
